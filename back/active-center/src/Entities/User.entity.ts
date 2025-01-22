db43a6fae16f97628c855311655a946c9ef9c84e
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-01-21 21:06:36 -0300

    Entidadde users sin terminar faltan relaciones.

--- back/active-center/src/Entities/User.entity.ts
+++ back/active-center/src/Entities/User.entity.ts
@@ -25,20 +25,20 @@ export class User {
   @Column({ type: 'varchar', length: 16, unique: true })
   phone: string;
 
-  @Column({ type: 'varchar', length: 50 })
-  country: string;
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  country?: string;
 
-  @Column({ type: 'text' })
-  address: string;
+  @Column({ type: 'text', nullable: true })
+  address?: string;
 
-  @Column({ type: 'varchar', length: 50 })
-  city: string;
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  city?: string;
 
-  @Column({type: 'number', length: 8, unique: true, nullable: false})
+  @Column({ type: 'number', length: 8, unique: true, nullable: false })
   dni: number;
 
-  @Column({type: 'enum', default: UserStatus.disconect, nullable: true})
-  userStatus: string
+  @Column({ type: 'enum', default: UserStatus.disconect, nullable: true })
+  userStatus: string;
 
   @Column({ type: 'boolean', default: false, nullable: true })
   isAdmin?: boolean;
