909d64ff8d25d4a421606e4d5d374200b4caca93
Ignacia22 <mfernandezpolanco@gmail.com>
2025-01-28 23:39:28 -0300

    correccion error de auth0-2

--- front/club-deportivo/src/components/Login/Login.tsx
+++ front/club-deportivo/src/components/Login/Login.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Link from "next/link";
-
+import { useRouter } from 'next/router';
 import React from "react";
 
 export default function Login() {
   const { user, error, isLoading } = useUser();
-
+  const router = useRouter();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
+  const handleLogout = () => {
+    router.push('/api/auth/logout');
+  };
+
+  const handleLogin = () => {
+    router.push('/api/auth/login');
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-[url('https://res.cloudinary.com/dqiehommi/image/upload/v1737912176/pexels-sukh-winder-3740393-5611633_y1bx8n.jpg')] bg-cover bg-center">
-      <form className="bg-black bg-opacity-80 p-8 rounded-lg shadow-md w-full max-w-lg">
+      <div className="bg-black bg-opacity-80 p-8 rounded-lg shadow-md w-full max-w-lg">
         <h2 className="text-3xl font-bold mb-6 text-white text-center">
           {user ? `Welcome ${user.name}!` : "Welcome Guest!"}
         </h2>
@@ -24,12 +31,12 @@ export default function Login() {
             <p className="text-white text-center mb-4">
               You are logged in as {user.email}
             </p>
-            <a
-              href="/api/auth/logout"
-              className="block text-center text-white bg-red-600 hover:bg-red-700 py-2 px-4 rounded transition font-bold"
+            <button
+              onClick={handleLogout}
+              className="w-full text-center text-white bg-red-600 hover:bg-red-700 py-2 px-4 rounded transition font-bold"
             >
               LOGOUT
-            </a>
+            </button>
           </>
         ) : (
           <>
@@ -51,18 +58,15 @@ export default function Login() {
               />
             </div>
 
-            <Link
-            href="/api/auth/logout"
-            passHref
-            legacyBehavior
+            <button
+              onClick={handleLogin}
+              className="w-full text-center text-white bg-blue-600 hover:bg-blue-700 py-2 px-4 rounded transition font-bold"
             >
-              <a className="block text-center text-white bg-red-600 hover:bg-red-700 py-2 px-4 rounded transition font-bold">
-                LOGOUT
-                </a>
-            </Link>
+              LOGIN
+            </button>
           </>
         )}
-      </form>
+      </div>
     </div>
   );
 }
