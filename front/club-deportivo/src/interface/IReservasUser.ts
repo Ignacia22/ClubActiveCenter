f5fccfcf70c97e1312a1846f342a98e82addf300
agustintoledo2908 <agustintoledo2908@gmail.com>
2025-01-29 21:56:49 -0300

    se hace la conexion con axios de registro

---
+++ front/club-deportivo/src/interface/IReservasUser.ts
@@ -0,0 +1,26 @@
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address?: string;
+  dni: string;
+  userStatus: string;
+  isAdmin: boolean;
+}
+
+export interface IActivity {
+  id: string;
+  name: string;
+  price: number;
+  status: boolean;
+}
+
+export interface IReservasUser {
+  id: string;
+  date: string;
+  status: boolean;
+  price: number;
+  user: IUser;
+  activities: IActivity[];
+}
