b46d2707085ac44ecfa648326f3a757044952fe9
agustintoledo2908 <agustintoledo2908@gmail.com>
2025-01-31 02:14:45 -0300

    se corrige auth0

--- front/club-deportivo/src/app/api/auth/[...auth0]/route.ts
+++ front/club-deportivo/src/app/api/auth/[...auth0]/route.ts
@@ -1,23 +1,5 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
 
-console.log("AUTH0_SECRET:", process.env.AUTH0_SECRET);
-console.log("AUTH0_BASE_URL:", process.env.AUTH0_BASE_URL);
-console.log("AUTH0_CLIENT_ID:", process.env.AUTH0_CLIENT_ID);
-console.log("AUTH0_CLIENT_SECRET:", process.env.AUTH0_CLIENT_SECRET);
-console.log("AUTH0_SECRET:", process.env.AUTH0_SECRET);
-console.log("AUTH0_BASE_URL:", process.env.AUTH0_BASE_URL);
-console.log("AUTH0_CLIENT_ID:", process.env.AUTH0_CLIENT_ID);
-console.log("AUTH0_CLIENT_SECRET:", process.env.AUTH0_CLIENT_SECRET);
-export const GET = handleAuth({
-  secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-});
+export const GET = handleAuth();
 
-export const POST = handleAuth({
-  secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-});
+export const POST = handleAuth();
