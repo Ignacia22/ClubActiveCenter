bf5bbf168b685f68d22cbe8ecc4ef881a9375777
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-02-02 22:49:54 -0300

    .

--- back/active-center/src/Auth/auth.service.ts
+++ back/active-center/src/Auth/auth.service.ts
@@ -253,10 +253,7 @@ export class AuthService {
   }
 
   async login({ email } : LoginDTO) {
-    console.log(email);
     const user: User | null = await this.userService.getUserByEmail(email);
-    console.log(user);
-    
     if (!user || user.userStatus === UserStatus.delete)
       throw new NotFoundException('No existe el usuario.');
     const token: string = this.jwtService.sign({
