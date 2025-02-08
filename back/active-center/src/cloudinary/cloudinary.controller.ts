ce0fc2fe3959eb7eac5cdc478960bc20d281d909
Richirix <ruizcalderonricardoed@gmail.com>
2025-02-08 12:51:47 -0600

    carga de imaganes url a productos 60 en total

---
+++ back/active-center/src/cloudinary/cloudinary.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Query, SetMetadata } from '@nestjs/common';
+import { CloudinaryService } from './cloudinary.service';
+
+@Controller('cloudinary')
+export class CloudinaryController {
+  constructor(private readonly cloudinaryService: CloudinaryService) {}
+   
+  
+  
+  @Get('images')
+  @SetMetadata('isPublic', true)
+  async getImages() {
+    const images = await this.cloudinaryService.getAllImagesFromCloudinary();
+    return images;
+  }
+}
--- back/active-center/src/cloudinary/cloudinary.module.ts
+++ back/active-center/src/cloudinary/cloudinary.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { CloudinaryConfig } from 'src/config/cloudinary';
+import { CloudinaryController } from './cloudinary.controller';
 
 @Module({
   providers: [CloudinaryService, CloudinaryConfig],
+  controllers: [CloudinaryController],
   exports: [CloudinaryService],
 })
 export class CloudinaryModule {}
