c9f269f6e40e6f47f218f45899cd3a7d9a0083f2
unknown <acarneri20@gmail.com>
2025-02-11 19:00:58 -0300

    ultimos detalles chat

--- back/active-center/src/webSockets/websocket.gateway.ts
+++ back/active-center/src/webSockets/websocket.gateway.ts
@@ -1,4 +1,4 @@
-import { NotFoundException} from "@nestjs/common";
+import { Inject, NotFoundException} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
@@ -10,6 +10,8 @@ import { Message } from "src/Entities/Message.entity";
 import { Repository } from "typeorm";
 import { Chat } from "src/Entities/Chat.entity";
 import { User } from "src/Entities/User.entity";
+import respuestasPredefinidas from "./resPredifinidas";
+import { SendGridService } from "src/SendGrid/sendGrid.service";
 
 @WebSocketGateway({cors:{origin:"*"}})
 export class websockets implements OnGatewayInit , OnGatewayConnection , OnGatewayDisconnect {
@@ -32,9 +34,7 @@ export class websockets implements OnGatewayInit , OnGatewayConnection , OnGatew
      async handleConnection(client: Socket) {
 
         try{
-            const token = client.handshake.auth.token;
-            console.log(client.handshake.auth)
-            console.log(client.handshake.auth.token);
+            const token = client.handshake.headers.authorization?.split(" ")[1] || client.handshake.auth.token;
             
             if(!token){
                 throw new NotFoundException("no se encontro el token")
@@ -43,7 +43,6 @@ export class websockets implements OnGatewayInit , OnGatewayConnection , OnGatew
             const payload = await this.jwtService.verifyAsync(token,{
                 secret: SECRET_SECRET_WORD 
             })
-            
             
             const user = await this.userService.getUserById(payload.id)
             if(!user){
@@ -68,39 +67,44 @@ export class websockets implements OnGatewayInit , OnGatewayConnection , OnGatew
         console.log('Usuarios conectados:', Array.from(this.users.keys()))
     }
 
+    private pendingMessages = new Map<string, Message[]>();
+    
     @SubscribeMessage("mensaje")
     async handleMessage(@ConnectedSocket() Client:Socket ,@MessageBody() data:CreateMessageDto){
 
         try{
-            const token = Client.handshake.auth.token;
+            const token =  Client.handshake.auth.token || Client.handshake.headers.authorization?.split(" ")[1];
             
             if(!token){
                 throw new NotFoundException("no se encontro el token")
             }
-
+            
             const payload = await this.jwtService.verifyAsync(token,{
                 secret: SECRET_SECRET_WORD 
             })
-            
             
             const user = await this.userRepository.findOne({
                 where:{id:payload.id},
                 relations:["chat"]
             })
+            
             if(!user){
                 throw new NotFoundException("el usuario no existe")
             }
-
-            const sender = payload.isAdmin;
+            
             const chatId = user.chat.id;
-              
             const chat = await this.chatRepository.findOne({
                 where:{id:chatId}
             })
             
             if(!chat){
-                throw new NotFoundException("el chat no existe");
+                const newChat = this.chatRepository.create({user})
+                
+                await this.chatRepository.save(newChat);
+                throw new NotFoundException("el usuario no tiene un chat asignado")
             }
+            
+            const sender = payload.isAdmin;
             
             const newMessage = this.messageRepository.create({
                 content:data.content,
@@ -110,23 +114,43 @@ export class websockets implements OnGatewayInit , OnGatewayConnection , OnGatew
             });
             
             await this.messageRepository.save(newMessage)
+            console.log(newMessage)
             
-            this.users.forEach((Client) => {
-                if((sender  && Client.data.user.id === user.id) || 
-                (!sender === true && Client.data.user.id === user.id)){
-                    
-                    Client.emit("mensajeserver" , newMessage)
+            this.server.emit("mensajeserver" , newMessage)
+            
+           function obtenerRespuestaAutomatica(mensaje: string): string | null {
+                const mensajeLower = mensaje.toLowerCase();
+                for (const { key, response } of respuestasPredefinidas) {
+                    if (key.some((palabra) => mensajeLower.includes(palabra))) {
+                        return response;
+                    }
                 }
-    
-         })
+                return null;
+            }
+            
+        
+        const respuestaBot = obtenerRespuestaAutomatica(data.content);
+        
+        if (respuestaBot) {
+        setTimeout(async () => {
+        const botMessage = this.messageRepository.create({
+            content: respuestaBot,
+            sender: true, 
+            createdAt: new Date(),
+            chat,
+        });
+        
+        await this.messageRepository.save(botMessage);
+        Client.emit("mensajeserver", botMessage);
+        }, 2000);
+        }
+
 
         }catch(error){
             console.log(error)
-        
+            
         }
+    }        
 
     }
-}
-
-
 
