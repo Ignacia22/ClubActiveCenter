b88803c847e3d136a1ead5899b4d6d880661b43b
Ignacia22 <mfernandezpolanco@gmail.com>
2025-02-12 14:15:59 -0300

    parte 1 de banear usuarios-2

--- front/club-deportivo/src/interface/IUser.ts
+++ front/club-deportivo/src/interface/IUser.ts
@@ -1,26 +1,26 @@
-import { UserStatus } from "@/components/InfoAdmin/UsersTable";
+import { UserStatus } from '@/components/InfoAdmin/UsersTable';
 
 export interface IUser {
   userInfo: {
-    id: string; // ID del usuario (UUID)
-    name: string; // Nombre del usuario
-    email: string; // Correo electrónico
-    phone: string; // Teléfono
-    address: string; // Dirección
-    userStatus: string; // Estado del usuario
-    isAdmin: boolean; // Si el usuario es admin
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    userStatus: string;
+    isAdmin: boolean;
   };
-  id: string; // ID global del usuario
+  id: string;
   name: string;
   email: string;
-  password: string; // Aunque no deberías exponerlo en el frontend
+  password: string;
   phone: string;
   address?: string;
-  dni: number; // DNI del usuario
-  activities: []; // Relación ManyToMany con Activity
-  reservations: []; // Relación OneToMany con Reservation
-  orders: []; // Relación OneToMany con Order
-  userStatus: UserStatus; // Tipo enum
+  dni: number;
+  activities: [];
+  reservations: [];
+  orders: [];
+  userStatus: UserStatus; // Usar el enum aquí
   isAdmin: boolean;
   createUser?: Date;
   updateUser?: Date;
