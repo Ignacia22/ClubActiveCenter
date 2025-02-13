550f75675c7342caaacfa1c3a766367242abf456
FlorGambero <florenciagambero12@gmail.com>
2025-02-12 23:31:31 -0300

    toque el navconfig defini una variable

--- front/club-deportivo/next.config.ts
+++ front/club-deportivo/next.config.ts
@@ -1,8 +1,8 @@
 import { NextConfig } from "next";
-
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
         protocol: "https",
@@ -14,11 +14,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/auth/:path*",
-        destination: "/api/auth/:path*", 
+        destination: "/api/auth/:path*",
       },
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${apiBaseUrl}/:path*`,
       },
     ];
   },
@@ -27,9 +27,9 @@ const nextConfig: NextConfig = {
     AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL, 
-    API_URL: process.env.API_URL, 
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, 
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
